3a8183881d46bc75817569fd5847b48d3f566b57
Anthony Lee <anthonylee@umass.edu>
2018-10-23 14:06:08 -0400

    first commit

---
+++ ProjectX/ViewControllers/FeedsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedsViewController.swift
+//  ProjectX
+//
+//  Created by Anthony Lee on 10/23/18.
+//  Copyright © 2018 projectX. All rights reserved.
+//
+
+import UIKit
+
+class FeedsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProjectX/ViewControllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectX
+//
+//  Created by Anthony Lee on 10/23/18.
+//  Copyright © 2018 projectX. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
